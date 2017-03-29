3cc5959160abd44b754acb47328a51fd82dc1218
xiaos <753623217@qq.com>
2017-03-29 12:04:20 +0800

    fix

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const KEY = config.weather_account.key;
 const Api = require('./api.js');
 const api = new Api(UID, KEY);
 
+const client = require('./DB');
+
+const collection = async (name)=>{
+    try{
+        const db = await client.get_client();
+        return db.collection(name)
+    }catch (err){
+        logger.error(`获取集合失败`);
+        return Promise.reject(err)
+    }
+};
+
 const http_post = (url,data)=>{
     return new Promise((s,f)=>{
         request.post(url, {form:data},(err,rs,body)=>{
@@ -19,9 +31,10 @@ const http_post = (url,data)=>{
 };
 
 (async()=>{
-    const tokens = require('./token.json');
+    const tokens = await collection('weibo_token');
 
-    const weather_task = tokens.map(data=>{
+    const token_list = await tokens.find();
+    const weather_task = token_list.map(data=>{
         return api.getWeatherNow(data.city);
     });
     const weather_data = await Promise.all(weather_task);
@@ -29,10 +42,10 @@ const http_post = (url,data)=>{
         const {location,now} = results[0];
         const {name} = location;
         const {text,temperature} = now;
-        return `中国 ${name}，今天的天气是${text} 当前温度是${temperature}℃ http://t.cn/R6f6HVt`;
+        return `中国 ${name}，今天的天气是${text}，当前温度是${temperature}℃ http://t.cn/R6f6HVt`;
     });
 
-    const tasks = tokens.map((data,index)=>{
+    const tasks = token_list.map((data,index)=>{
         return http_post('https://api.weibo.com/2/statuses/update.json',{
             access_token:data.access_token,
             status:weibo_datas[index]
